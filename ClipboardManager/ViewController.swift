594fecbfcd5fc1d3a235b900a7ad912900c1a763
이기완 <kiwan0930@gmail.com>
2023-10-05 16:52:30 +0900

    Initial Commit

---
+++ ClipboardManager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ClipboardManager
+//
+//  Created by 이기완 on 10/5/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
